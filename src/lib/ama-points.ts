706692ce6bfe453625c7d99349bdf45a9276bc66
FUCKiro <fabio.rock84@gmail.com>
2025-02-10 15:29:08 +0100

    Add directions functionality

--- src/lib/ama-points.ts
+++ src/lib/ama-points.ts
@@ -2,19 +2,21 @@
 export const amaPoints = [
   {
     name: "Centro di raccolta Ama - Acqua Acetosa",
-    address: "via Campi Sportivi, 100",
+    address: "Via dei Campi Sportivi, 100",
     latitude: 41.9283,
     longitude: 12.4876,
     hours: "Giorni feriali: 7.00 - 12.00 e 14.00 - 19.00\nDomenica: 07.00 - 13.00",
-    municipio: "II"
+    municipio: "II",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.9283,12.4876"
   },
   {
     name: "Centro di raccolta Ama - Bufalotta",
-    address: "Via della Bufalotta 592",
+    address: "Via della Bufalotta, 592",
     latitude: 41.9569,
     longitude: 12.5451,
     hours: "Giorni feriali: orario continuato 07.30 - 18.30\nDomenica: 07.00 - 13.00",
-    municipio: "III"
+    municipio: "III",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.9569,12.5451"
   },
   {
     name: "Centro di raccolta Ama - Vigne Nuove",
@@ -22,17 +24,19 @@ export const amaPoints = [
     latitude: 41.9486,
     longitude: 12.5367,
     hours: "Giorni feriali: orario continuato 07.30 - 18.30\nDomenica: 07.00 - 13.00",
-    municipio: "III"
+    municipio: "III",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.9486,12.5367"
   },
   {
     name: "Centro di raccolta Ama - Tiburtina",
-    address: "Via Cassino, 7-9",
+    address: "Via di Cassino, 7-9",
     latitude: 41.9144,
     longitude: 12.5489,
     hours: "Giorni feriali: orario continuato 07.30 - 18.30\nDomenica: 07.00 - 13.00",
     municipio: "IV",
     notes: "Chiuso dal 07/10/2024 al 30/04/2025",
-    metro: "Metro B Ponte Mammolo"
+    metro: "Metro B Ponte Mammolo",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.9144,12.5489"
   },
   {
     name: "Centro di raccolta Ama - Villa Gordiani",
@@ -40,31 +44,35 @@ export const amaPoints = [
     latitude: 41.8932,
     longitude: 12.5432,
     hours: "Giorni feriali: orario continuato 07.30 - 17.00\nDomenica: 07.00 - 13.00",
-    municipio: "V"
+    municipio: "V",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.8932,12.5432"
   },
   {
     name: "Centro di raccolta Ama - Casale Cerroncino",
-    address: "via Casale Cerroncino 36",
+    address: "Via del Casale Cerroncino, 36",
     latitude: 41.8789,
     longitude: 12.5912,
     hours: "Giorni feriali: orario continuato 07.30 - 18.30\nDomenica: 07.00 - 13.00",
-    municipio: "VI"
+    municipio: "VI",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.8789,12.5912"
   },
   {
     name: "Centro di raccolta Ama - Cinecittà",
-    address: "Viale P. Togliatti, 69",
+    address: "Viale Palmiro Togliatti, 69",
     latitude: 41.8636,
     longitude: 12.5644,
     hours: "Giorni feriali: orario continuato 07.30 - 18.30\nDomenica: 07.00 - 13.00",
-    municipio: "VII"
+    municipio: "VII",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.8636,12.5644"
   },
   {
     name: "Centro di raccolta Ama - Mostacciano",
-    address: "via Riccardo Boschiero",
+    address: "Via Riccardo Boschiero",
     latitude: 41.8132,
     longitude: 12.4673,
     hours: "Giorni feriali: orario continuato 07.30 - 18.30\nDomenica: 07.00 - 13.00",
-    municipio: "IX"
+    municipio: "IX",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.8132,12.4673"
   },
   {
     name: "Centro di raccolta Ama - Laurentina",
@@ -73,7 +81,8 @@ export const amaPoints = [
     longitude: 12.4821,
     hours: "Giorni feriali: orario continuato 07.30 - 18.30\nDomenica: 07.00 - 13.00",
     municipio: "IX",
-    notes: "Direzione centro, prossimità incrocio via di Tor Pagnotta"
+    notes: "Direzione centro, prossimità incrocio via di Tor Pagnotta",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.8235,12.4821"
   },
   {
     name: "Centro di raccolta Ama - Acilia",
@@ -82,23 +91,26 @@ export const amaPoints = [
     longitude: 12.3531,
     hours: "Giorni feriali: orario continuato 07.30 - 18.30\nDomenica: 07.00 - 13.00",
     municipio: "X",
-    notes: "Angolo Via D. Morelli"
+    notes: "Angolo Via D. Morelli",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.7553,12.3531"
   },
   {
     name: "Centro di raccolta Ama - Lido di Ostia",
-    address: "Piazza Bottero 8",
+    address: "Piazza Bottero, 8",
     latitude: 41.7321,
     longitude: 12.2789,
     hours: "Giorni feriali: orario continuato 07.30 - 18.30\nDomenica: 07.00 - 13.00",
-    municipio: "X"
+    municipio: "X",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.7321,12.2789"
   },
   {
     name: "Centro di raccolta Ama - Corviale",
-    address: "via Arturo Martini, 1",
+    address: "Via Arturo Martini, 1",
     latitude: 41.8432,
     longitude: 12.3921,
     hours: "Giorni feriali: orario continuato 07.30 - 18.30\nDomenica: 07.00 - 13.00",
-    municipio: "XI"
+    municipio: "XI",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.8432,12.3921"
   },
   {
     name: "Centro di raccolta Ama - Battistini",
@@ -106,7 +118,8 @@ export const amaPoints = [
     latitude: 41.9021,
     longitude: 12.4112,
     hours: "Giorni feriali: orario continuato 07.30 - 18.30\nDomenica: 07.00 - 13.00",
-    municipio: "XIV"
+    municipio: "XIV",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=41.9021,12.4112"
   },
   {
     name: "Raccolta sfalci e potature - La Storta",
@@ -115,6 +128,7 @@ export const amaPoints = [
     longitude: 12.3789,
     hours: "Giorni feriali: 07.00 - 12.00 e 14.00 - 18.00\nDomenica: 07.00 - 12.00",
     municipio: "XV",
-    notes: "È consentito consegnare al massimo 4 sacchi da 70 lt al giorno per autovettura.\nChiuso dal 11/11/2024 al 30/06/2025"
+    notes: "È consentito consegnare al massimo 4 sacchi da 70 lt al giorno per autovettura.\nChiuso dal 11/11/2024 al 30/06/2025",
+    googleMapsUrl: "https://www.google.com/maps/dir/?api=1&destination=42.0012,12.3789"
   }
 ];
